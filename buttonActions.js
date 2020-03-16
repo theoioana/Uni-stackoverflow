98aa19fb0c0ee2c6d80a111a79e1391cf071316a
Theodora Danciulescu <theodora_danciulescu@yahoo.com>
2020-03-17 01:08:40 +0200

    Major commit
    Unfied internal page styles, modified account page, added my questions page

--- buttonActions.js
+++ buttonActions.js
@@ -9,3 +9,11 @@
   function GoToHomePage() {
     document.location.href("HomePage.html")
   }
+  
+  function GoToMyQuestionsPage() {
+    document.location.href("MyQuestions.html")
+  }
+  
+  function GoToAccountPage() {
+    document.location.href("Account.html")
+  }
